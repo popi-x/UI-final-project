df061adbd7fcc2a9d9f4059b7e832d02671f678b
popi-x <ssincn@outlook.com>
2025-05-12 20:15:23 -0400

    add alternative descriptions to shutter speed image

--- Static/js/shutter_speed.js
+++ Static/js/shutter_speed.js
@@ -5,7 +5,9 @@ $(document).ready(function() {
         
         let img_index = parseInt($(this).data("img"), 10);
         let source = "/static/" + data.images[img_index];
+        let alternate = data.alt[img_index];
         $("#sample-img").attr("src", source);
+        $("#sample-img").attr("alt", alternate);
         $("#sample-img").show();
         $(".shutter-speed-title").removeClass("active");
         $(this).addClass("active");
